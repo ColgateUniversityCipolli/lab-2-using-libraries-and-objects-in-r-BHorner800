4ec55820d731212c3dac9d7905fd4daf5dc18b50
BHorner800 <benjamin.r.horner@gmail.com>
2025-02-06 11:59:09 -0500

    Lab 2 Updated
    Test File is done, writing up in Rnw file.

--- TestFile.R
+++ TestFile.R
@@ -10,11 +10,11 @@
 library(stringr)
 
 #Step 1
+?str_split_i
 music.dirs <- list.dirs("MUSIC")
-
-curr.album = music.dirs[3]
-dir.level.test = str_count(curr.album, pattern = "/")
-
+#Test
+#curr.album = music.dirs[3]
+#dir.level.test = str_count(curr.album, pattern = "/")
 #Step 2
 artist.dirs = c()
 album.dirs = c()
@@ -32,39 +32,68 @@ print(artist.dirs)
 print(album.dirs)
 
 #Step 3
+#Test
 album.dirs[1]
-print(str_split(album.dirs[1], pattern = "/"))
+test = str_split(album.dirs[1], pattern = "/", simplify = TRUE)
+print(test)
+print(test[1,2])
 ##3.1-3.2
 #wav.files.total = list()
-wav.files = c()
+json.files = c()
 for (album in album.dirs){
-  album.name = str_split_i(album, pattern = "/", -1)
-  artist.name = str_split_i(album, pattern = "/", -2)
-  test = paste(artist.name, album.name, sep = "-")
+  ###Splits the string of the directory into Music, Artist, Album
+  split.string = str_split(album, pattern = "/", simplify = TRUE)
+  album.name = split.string[1, 3]
+  artist.name = split.string[1, 2]
+  art.n.album = paste(artist.name, album.name, sep = "-") #artist and album
+                                                          #comined in desired format
+
+  ##Lists the music files in each album, goes through them, creates the final
+  ##desired string of [artist name]-[album name]-[trackname].json
   music.files <- list.files(album)
   for (music.file in music.files){
-    #print(music.file)
     if (str_count(music.file, pattern = ".wav")){
-      file.path = str_split_i(music.file, pattern = ".wav", -2)
-      file.path = str.split_i(file.path, pattern = "-", -2)
-      print(file.path)
-      final.path = paste(test, "-", file.path, ".json", sep = "")
-      wav.files = append(wav.files, final.path) #will contain all of the .wav files in all of the albums 
+      #Makes sure we are only dealing with .wav files
+      file.path = str_split(music.file, pattern = ".wav", simplify =  TRUE)
+      file.path = str_split(file.path, pattern = "-", simplify = TRUE)
+      song.name = file.path[1, 3]
+      final.path = paste(art.n.album, "-", song.name, ".json", sep = "")
+      json.files = append(wav.files, final.path) #will contain all of the .wav files in all of the albums 
     }
-  #print(wav.files)
   }
-  #wav.files.total = append(wav.files.total, wav.files)
 }
-print(wav.files)
+print(json.files)
 
-##3.3
-code.to.process = c()
-###3.3a
-desired_output = c() # [artist name]-[album naame]-[track name].json
+#3d
+code.to.process = paste(json.files, "streaming_extractor_music.exe")
+print(code.to.process)
 
-for (file in wav.files){
-  track.name = str_split(file, pattern = ".", simplify = TRUE)
-  print(track.name)
-  #track.names.location = append(track.names.location, )
-}
-?str_split
+
+###############################################
+#Task 2: Process JSON Output
+###############################################
+
+#Step 0
+library(jsonlite)
+
+#Step 1
+json.file = (list.files())[8] 
+#gives us the file we want. In the future, we can just loop through the .json
+#files that we have in a list or folder
+split.file = str_split(json.file, "-", simplify = TRUE)
+artist.name = split.file[1, 1]
+album = split.file[1, 2]
+song.name = split.file[1, 3] #this still has .json on it
+song.name = str_split(song.name, ".json", simplify = TRUE)[1,1]
+
+#Step 2
+json.data = fromJSON(json.file)
+
+#Step 3
+average.loudness = json.data$lowlevel$average_loudness
+mean.spectral.enrgy = json.data$lowlevel$spectral_energy$mean
+danceability = json.data$rhythm$danceability
+bpm = json.data$rhythm$bpm
+musical.key = json.data$tonal$key_key
+musical.mode = json.data$tonal$key_scale
+#duration = json.data ####in seconds, cant find right now.
